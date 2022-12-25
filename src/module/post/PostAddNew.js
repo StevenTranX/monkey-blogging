62100049dd4d8810b8be619ddeb2b64cf4f8994d
Steven-Tran <trantrongtinnna3k55@gmail.com>
2022-12-25 20:47:18 +0700

    upload image

--- src/module/post/PostAddNew.js
+++ src/module/post/PostAddNew.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
@@ -8,23 +7,117 @@ import { Field } from "../../components/field";
 import { Input } from "../../components/input";
 import { Label } from "../../components/label";
 import Radio from "../../components/radio/Radio";
+import slugify from "slugify";
+import { postStatus } from "../../utils/constants";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
+import { toast } from "react-toastify";
+import ImageUpload from "../../components/image/ImageUpload";
+import { useState } from "react";
+import { addDoc, collection, deleteDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase-config";
 const PostAddNewStyles = styled.div``;
-
 const PostAddNew = () => {
-  const { control, watch, setValue } = useForm({
+  const { control, watch, setValue, handleSubmit, getValues } = useForm({
     mode: "onChange",
     defaultValues: {
-        status: "",
-        category: "",
-      },
+      title: "",
+      slug: "",
+      status: 2,
+      category: "",
+    },
   });
   const watchStatus = watch("status");
   const watchCategory = watch("category");
-  console.log("PostAddNew ~ watchCategory", watchCategory);
+  const storage = getStorage();
+  const [progress, setProgress] = useState(0);
+  const [image, setImage] = useState("");
+  const addPostHandler = async (values) => {
+    const cloneValues = { ...values };
+    cloneValues.slug = slugify(cloneValues.title || cloneValues.slug);
+    cloneValues.status = Number(cloneValues.status);
+    // const colRef = collection(db, 'posts');
+    // await addDoc(colRef,{
+    //   image,
+    //   ...
+    // })
+    // handleUploadImage(values.image);
+  };
+  const handleUploadImage = (file) => {
+    const storageRef = ref(storage, "images/" + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progressPercent =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // progress 5% 10% 15 %
+        setProgress(progressPercent);
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            console.log("Nothing at all");
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            toast("Unauthorized");
+            break;
+          case "storage/canceled":
+            toast("Canceled");
+            break;
+          case "storage/unknown":
+            toast("Unknown Error");
+            break;
+          default:
+            console.log("nothing at all");
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImage(downloadURL);
+        });
+      }
+    );
+  };
+  // * dùng hàm này khi submit
+  const onSelectImage = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue("image_name", file.name);
+    handleUploadImage(file);
+  };
+  const handleDeleteImage = async () => {
+    const storage = getStorage();
+    const desertRef = ref(storage, `images/${getValues("image_name")}`);
+    await deleteObject(desertRef)
+      .then(() => {
+        toast("File Deleted Successfully");
+        setImage('')
+        setProgress(0)
+      })
+      .catch(() => {
+        toast("Oops! an error occurred");
+      });
+  };
   return (
     <PostAddNewStyles>
       <h1 className="dashboard-heading">Add new post</h1>
-      <form>
+      <form onSubmit={handleSubmit(addPostHandler)}>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
             <Label>Title</Label>
@@ -32,6 +125,7 @@ const PostAddNew = () => {
               control={control}
               placeholder="Enter your title"
               name="title"
+              required
             ></Input>
           </Field>
           <Field>
@@ -42,6 +136,18 @@ const PostAddNew = () => {
               name="slug"
             ></Input>
           </Field>
+          <Field>
+            <Label>Image</Label>
+            <ImageUpload
+              handleDeleteImage={handleDeleteImage}
+              type="file"
+              onChange={onSelectImage}
+              className="h-[250px]"
+              name="image"
+              progress={progress}
+              image={image}
+            />
+          </Field>
         </div>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
@@ -50,25 +156,27 @@ const PostAddNew = () => {
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "approved"}
-                value="approved"
+                checked={+watchStatus === postStatus.APPROVED}
+                value={postStatus.APPROVED}
               >
                 Approved
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "pending"}
-                value="pending"
+                checked={+watchStatus === postStatus.PENDING}
+                value={postStatus.PENDING}
               >
                 Pending
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === "reject"}
-                value="reject"
+                checked={+watchStatus === postStatus.REJECTED}
+                // watchStatus tự động convert number sang string '3' nên ta để dấu + để convert sang number
+                value={postStatus.REJECTED}
               >
+                {/* 1 là approved 2 là pending 3 là reject  */}
                 Reject
               </Radio>
             </div>
